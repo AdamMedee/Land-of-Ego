e1f0a539159253f4d52e9fb5b1a2a8b475c2333f
Adam Mehdi <adammehdi98765@gmail.com>
2023-10-23 05:11:18 -0400

    Added UI elements along with simple navigation
    UI elements, play settings quit and back buttons along with their animations implemented

---
+++ Assets/Scripts/UI/UIButtons.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButtons : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            transform.localScale = new Vector3(1, 1, 0.3f);
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            transform.localScale = new Vector3(1, 1, 0.6f);
+        }
+        else
+        {
+            transform.localScale = new Vector3(1, 1, 0.6f);
+        }
+    }
+
+    void OnMouseExit()
+    {
+        transform.localScale = new Vector3(1, 1, 1);
+    }
+}
